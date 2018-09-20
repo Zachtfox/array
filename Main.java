256055db8b3062f2f3e553255903f44e37464816
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2018-09-20 10:35:22 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] iceCream = {"Chocolate","Cookie Dough", "Rocky Road", "Cookies and Cream", "Moose Tracks"};
+        for(int i = 0; i < iceCream.length; i++)
+        System.out.println(iceCream[i]);
+    }
+}
